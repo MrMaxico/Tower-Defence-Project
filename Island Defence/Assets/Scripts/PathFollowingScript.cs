f16715aba0918497c569c4eb64600412559b1853
NickSwitchSchool <97087180@st.deltion.nl>
2022-09-02 14:24:57 +0200

    Enemy follow path
    Enemy kan nu een pad volgen aan de hand van een array met punten

---
+++ Island Defence/Assets/Scripts/PathFollowingScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+
+public class PathFollowingScript : MonoBehaviour
+{
+    public Transform[] path;
+
+    public float speed;
+
+    public int pathProgress;
+
+    bool onReturn;
+
+    private void Start()
+    {
+        for (int i = 0; i < path.Length - 4; i++)
+        {
+            Debug.DrawLine(path[i].position, path[i + 1].position, Color.red, 6000f);
+        }
+    }
+
+    private void Update()
+    {
+        //check if enemy is at his goal
+        if (transform.position == path[path.Length - 1].position)
+        {
+            StartCoroutine(PickupGem());
+        }
+
+        //path of the enemy
+        if (gameObject.transform.position != path[pathProgress].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, path[pathProgress].position, speed * Time.deltaTime);
+        }
+        else if (!onReturn && pathProgress < path.Length - 1)
+        {
+            pathProgress++;
+        }
+        else if (onReturn &&pathProgress > 0)
+        {
+            pathProgress--;
+        }
+    }
+
+    private IEnumerator PickupGem()
+    {
+        yield return new WaitForSeconds(1);
+        onReturn = true;
+    }
+}
