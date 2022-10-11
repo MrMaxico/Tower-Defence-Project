47fdce6c036ed25e8dd7a9d1cdef16496904261c
MrMaxico <mr.maxico4@gmail.com>
2022-10-11 16:36:52 +0200

    audiomanager script gemaakt
    dank brackeys

--- Island Defence/Assets/Scripts/Max/AudioManagerScript.cs
+++ Island Defence/Assets/Scripts/Max/AudioManagerScript.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 public class AudioManagerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Sound[] sounds;
+
+    public static AudioManagerScript instance;
+    private void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.outputAudioMixerGroup = s.group;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void play (string name)
     {
-        
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.Play();
     }
+
+
+
+    //deze line code hieronder moet je gcopy pasten als je een geluidje wilt afspelen
+    //FindObjectOfType<AudioManagerScript>().Play("naamvangeluidhier");
 }
